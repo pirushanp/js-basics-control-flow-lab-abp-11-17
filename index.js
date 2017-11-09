ec53ccef3c6922f79f5a29bcc7bc54ffed7fbdd8
pirushanp <pirushanp@gmail.com>
2017-11-09 09:35:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Write your code in this file!
 function scuberGreetingForFeet(){
-  let scuberGreetingForFeet = 199
+  var scuberGreetingForFeet = 199
   if (scuberGreetingForFeet <= 400){
     return "This one is on me!"
   }
-  let scuberGreetingForFeet = 2001
+  var scuberGreetingForFeet = 2001
   if (scuberGreetingForFeet >= 2001){
     return "I will gladly take your thirty bucks."
   }
