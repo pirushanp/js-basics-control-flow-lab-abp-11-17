aeb695721083fd8d2a4640766e3df4f4b1c13172
pirushanp <pirushanp@gmail.com>
2017-11-09 09:27:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+function scuberGreetingForFeet(){
+  const scuberGreetingForFeet = 199
+  if (scuberGreetingForFeet <= 400){
+    return "This one is on me!"
+  }
+}
