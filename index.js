a780c1a219f7e3943ad73701e8cb6fdc0b58e833
pirushanp <pirushanp@gmail.com>
2017-11-09 09:30:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Write your code in this file!
 function scuberGreetingForFeet(){
-  const scuberGreetingForFeet = 199
   if (scuberGreetingForFeet <= 400){
+    const scuberGreetingForFeet = 199
     return "This one is on me!"
   }
+  else if ()
 }
