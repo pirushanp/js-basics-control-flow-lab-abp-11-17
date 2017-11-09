f8e6913dc58b3e2dad090953163309c43ff25e2e
pirushanp <pirushanp@gmail.com>
2017-11-09 09:37:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function scuberGreetingForFeet(){
   var scuberGreetingForFeet = 199
   if (scuberGreetingForFeet <= 400){
     return "This one is on me!"
-  
+  }
   var scuberGreetingForFeet = 2001
   if (scuberGreetingForFeet >= 2001){
     return "I will gladly take your thirty bucks."
