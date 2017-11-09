bea1bd2431e1beed8356ae3b4fb1d646e0911068
pirushanp <pirushanp@gmail.com>
2017-11-09 09:34:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 // Write your code in this file!
 function scuberGreetingForFeet(){
-  const scuberGreetingForFeet = 199
+  let scuberGreetingForFeet = 199
   if (scuberGreetingForFeet <= 400){
     return "This one is on me!"
   }
-}
-function scuberGreetingForFeet(){
-  const scuberGreetingForFeet = 2001
+  let scuberGreetingForFeet = 2001
   if (scuberGreetingForFeet >= 2001){
     return "I will gladly take your thirty bucks."
   }
